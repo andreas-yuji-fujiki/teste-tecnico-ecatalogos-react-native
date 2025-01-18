fc600268bf193ad865cc738af85b15eeb56c326a
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-01-18 09:34:03 -0300

    feat(info card): created

---
+++ app/components/atoms/InfoCard.tsx
@@ -0,0 +1,64 @@
+// imports
+import { useState, useEffect } from "react";
+import styled from "styled-components/native";
+
+// types
+import { Product as ProductType } from '../../../types/ProductTypes';
+
+interface InfoCardProps {
+    product: ProductType;
+    display: "hidden" | "visible";
+}
+
+// component
+const InfoCard = ({ product, display }: InfoCardProps) => {
+    // visibility state
+    const [displayClass, setDisplayClass] = useState<"none" | "flex">("none");
+
+    // updates visibility when 'display' changes
+    useEffect(() => {
+        setDisplayClass(display === "hidden" ? "none" : "flex");
+    }, [display]);
+
+    // display styles
+    const containerStyle = {
+        display: displayClass,
+    };
+
+    return (
+        <InfoCardContainer style={containerStyle}>
+            <Header>
+                <HeaderTitle>
+                    Informações
+                </HeaderTitle>
+            </Header>
+            <InfoContainer>
+                <InfoText>
+                    Nome do produto: {product.name}
+                </InfoText>
+                <InfoText>
+                    Referência: {product.reference}
+                </InfoText>
+                <InfoText>
+                    Marca: {product.brand}
+                </InfoText>
+                <InfoText>
+                    Categoria: {product.category}
+                </InfoText>
+                <InfoText>
+                    Gênero: {product.gender}
+                </InfoText>
+            </InfoContainer>
+        </InfoCardContainer>
+    );
+};
+export default InfoCard;
+
+// styled components
+const InfoCardContainer = styled.View``;
+
+const Header = styled.View``;
+const HeaderTitle = styled.Text``;
+
+const InfoContainer = styled.View``;
+const InfoText = styled.Text``;
---
+++ types/ProductTypes.d.ts
@@ -0,0 +1,37 @@
+type Product = {
+    id: number;
+    name: string;
+    reference: string;
+    gender: string;
+    category: string;
+    subcategory: string | null;
+    prompt_delivery: boolean;
+    skus: Sku[];
+    companies: {
+      key: number;
+    };
+    brand: string;
+    price: number;
+    colors: any[];
+    images: Image[];
+  };
+  
+  type Sku = {
+    id: number;
+    size: string;
+    stock: number;
+    open_grid: boolean;
+    min_quantity: number;
+  };
+  
+  type Image = {
+    id: number;
+    order: number;
+    path: string;
+  };
+  
+  type ProductsResponse = {
+    products: Product[];
+  };
+  
+  export type { Product, Sku, Image };
