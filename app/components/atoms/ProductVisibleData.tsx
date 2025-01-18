c969c01eaa98fe442ed70d8440347f71dbfec3cd
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-01-18 10:46:45 -0300

    feat(product visible data): created

---
+++ app/components/atoms/ProductVisibleData.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components/native";
+import { Product as ProductData } from '../../../types/ProductTypes'
+
+interface VisibleDataProps {
+    product: ProductData;
+}
+
+const ProductVisibleData = ( { product } : VisibleDataProps ) => {
+    return (
+        <DataContainer>
+            <DataText>
+                { product.name }
+            </DataText>
+            <DataText>
+                REF: {product.reference}
+            </DataText>
+            <DataText>
+                R$ { product.price }
+            </DataText>
+        </DataContainer>
+    );
+};
+export default ProductVisibleData;
+
+const DataContainer = styled.View``;
+const DataText = styled.Text``;
