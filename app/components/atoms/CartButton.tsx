ea38837e3ae1752bf46b623fea91fe69371e8b13
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-01-18 08:54:47 -0300

    micro refactor

--- app/components/atoms/CartButton.tsx
+++ app/components/atoms/CartButton.tsx
@@ -1,16 +1,15 @@
 import styled from "styled-components/native";
 
-import { Image } from "react-native";
-
 import CartButtonIcon from '../../../assets/images/icons/cart-button-icon.svg'
 
 const CartButton = () => {
-    return(
+    return (
         <ButtonWrapper>
-            <Image source={CartButtonIcon} />
+            <ButtonIcon source={CartButtonIcon} />
         </ButtonWrapper>
     );
 };
 export default CartButton;
 
 const ButtonWrapper = styled.TouchableOpacity``;
+const ButtonIcon = styled.Image``;
