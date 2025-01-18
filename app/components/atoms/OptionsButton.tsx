af05b90c9a0f585726ef6dc27bf529bcd930b314
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-01-18 07:36:23 -0300

    feat(options button): created

---
+++ app/components/atoms/OptionsButton.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native'
+import { Image } from 'react-native'
+
+import Colors from '@/app/constants/Colors'
+import OptionsButtonIcon from '../../../assets/images/icons/options-button-icon.svg'
+
+export default function OptionsButton() {
+  return (
+    <OptionsButtonButtonWrapper>
+        <Image source={OptionsButtonIcon} />
+    </OptionsButtonButtonWrapper>
+  )
+}
+
+const OptionsButtonButtonWrapper = styled.TouchableOpacity`
+    background-color: ${( Colors.blueGray )};
+`
